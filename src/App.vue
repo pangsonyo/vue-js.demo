9d9f55da495b649a4f409cb56cb3b3763648749d
pangsonyo <1015614034@qq.com>
2018-04-11 23:29:45 +0800

    vue-demo

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="1" style="margin-left:0px;">处理中心</el-menu-item>
+          <el-submenu index="2" style="margin-left:300px;">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" style="margin-left:300px;" disabled>消息中心</el-menu-item>
+          <el-menu-item index="4" style="margin-left:300px;"><a href="https://www.ele.me" style="text-decoration: none"
+                                                               target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
+      </el-header>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>这是尾巴</el-footer>
+    </el-container>
+
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+
+    //name: 'App'
+  }
+</script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #545c64;
+    color: #333;
+    text-align: center;
+    line-height: 80px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 480px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+</style>
