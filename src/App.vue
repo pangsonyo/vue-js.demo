2c5c4c1dd5bc76053b885534c13dbf36c3af38c2
xuanwen <906252481@qq.com>
2018-08-16 15:10:47 +0800

    1111

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,84 @@
           class="el-menu-demo"
           mode="horizontal"
           @select="handleSelect"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
+          background-color=""
+          text-color="red"
+          active-text-color="black">
           <el-menu-item index="1" style="margin-left:0px;">处理中心</el-menu-item>
-          <el-submenu index="2" style="margin-left:300px;">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3" style="margin-left:300px;" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4" style="margin-left:300px;"><a href="https://www.ele.me" style="text-decoration: none"
-                                                               target="_blank">订单管理</a></el-menu-item>
+          <el-menu-item index="2" style="">1231231</el-menu-item>
+          <el-menu-item index="3" style="background-color:rosybrown">消息中心</el-menu-item>
+          <el-menu-item index="4"style="background-color: blanchedalmond">XXXXxx</el-menu-item>
         </el-menu>
       </el-header>
 
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-      <el-footer>这是尾巴</el-footer>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu :default-openeds="['1', '3']" background-color="">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>导航一</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <el-table :data="tableData"
+                    tooltip-effect="dark"
+                    style="width: 100%"
+                    @selection-change="handleSelectionChange">
+            <el-table-column
+              type="selection"
+              width="55">
+            </el-table-column>
+            <el-table-column prop="date" label="日期" width="140">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="地址">
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
     </el-container>
 
     <!--<router-view/>-->
@@ -44,15 +96,49 @@
   export default {
 
     data() {
+
       return {
-        activeIndex: '1',
-        activeIndex2: '1'
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
       };
     },
     methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+     }
     }
 
     //name: 'App'
@@ -60,19 +146,18 @@
 </script>
 
 <style>
-  .el-header, .el-footer {
-    background-color: #545c64;
-    color: #333;
+  .el-header {
+    background-color:black;
+    color: black;
     text-align: center;
     line-height: 80px;
   }
 
-  .el-main {
+/*  .el-main {
     background-color: #E9EEF3;
     color: #333;
-    text-align: center;
-    line-height: 480px;
-  }
+    line-height: 800px;
+  }*/
 
   body > .el-container {
     margin-bottom: 40px;
